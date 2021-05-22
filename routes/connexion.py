700105436722310238128b367ce097f0fe40b8de
Erwan <kerbrat.erwan@gmail.com>
2021-05-22 19:49:29 +1100

    mise en cohérence avec les autres repos

--- routes/connexion.py
+++ routes/connexion.py
@@ -3,27 +3,40 @@ from flask_restful import Api, Resource, reqparse
 
 
 connexion1_post_args = reqparse.RequestParser()
-connexion1_post_args.add_argument("param1",type=str,required=True)
-connexion1_post_args.add_argument("param2",type=str,required=True)
+connexion1_post_args.add_argument("user",type=str,required=True)
+connexion1_post_args.add_argument("password",type=str,required=True)
 connexion2_post_args = reqparse.RequestParser()
-connexion2_post_args.add_argument("param3",type=int,required=True)
-connexion2_post_args.add_argument("param4",type=int,required=True)
+connexion2_post_args.add_argument("Nom",type=str,required=True)
+connexion2_post_args.add_argument("Prenom",type=str,required=True)
 
 
 class connexion1(Resource):
 	def post(self): #s_inscrire
 		body = connexion1_post_args.parse_args()
-		[param1,param2] = [body[i] for i in body]
+		[user,password] = [body[i] for i in body]
 
+		users = {"Louise":"1234","Erwan":"4321"}
 
-		return param1+param2
+		if user in users:
+
+			if users[user] == password:
+				return "ok"
+				
+		return "pas ok"
+
 
 
 class connexion2(Resource):
 	def post(self): #verifier_son_adresse_mail
 		body = connexion2_post_args.parse_args()
-		[param3,param4] = [body[i] for i in body]
+		[Nom,Prenom] = [body[i] for i in body]
 
-		return param3+param4
+		personnes = [{"Nom":"Kerbrat","Prenom":"Erwan","Age":24,"Partenaire":"Alizée"},
+					 {"Nom":"Leroux","Prenom":"Louise","Age":45,"Partenaire":"Auresh"}]
 
+		for i in range(len(personnes)):
+			if personnes[i]["Nom"]==Nom and personnes[i]["Prenom"]==Prenom:
+				return personnes[i]["Partenaire"]	 
+
+		return "none"
 
